2a6ce07d1949541ae107e2f6daed259f386ad477
Tejas Musale <tejasmusale99@gmail.com>
2025-07-08 23:38:42 +0530

    created responsive tasklist

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,11 +1,13 @@
 import Header from "../smallComponents/Header"
 import { TaskStatusPanel } from "../smallComponents/TaskStatusPanel"
+import { TaskList } from "../TaskList/TaskList"
 
 const EmployeeDashboard = () => {
     return(
-        <div className="dashboard p-10 bg-[#1c1c1c] h-screen">
+         <div className="dashboard min-h-screen p-10 bg-[#1c1c1c]">
             <Header />
             <TaskStatusPanel />
+            <TaskList />
         </div>
     )
 }
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,26 @@
+export const TaskList = () => {
+    return(
+        <div className="overflow-x-auto whitespace-nowrap no-scrollbar">
+            <div className="flex h-[250px] gap-5 items-center justify-center ">
+                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
+
+                </div>
+                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
+
+                </div>
+                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
+
+                </div>
+                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
+
+                </div>
+                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
+
+                </div>
+                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
+
+                </div>
+            </div>
+        </div>
+    )
+}
