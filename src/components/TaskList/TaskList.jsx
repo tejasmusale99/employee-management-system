7a9a277931a2110ae1b45bea3c6fa989fe714db1
Tejas Musale <tejasmusale99@gmail.com>
2025-07-09 22:16:02 +0530

    Ui improvement and responsivness

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,26 +1,68 @@
 export const TaskList = () => {
-    return(
-        <div className="overflow-x-auto whitespace-nowrap no-scrollbar">
-            <div className="flex h-[250px] gap-5 items-center justify-center ">
-                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
-
-                </div>
-                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
-
-                </div>
-                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
-
-                </div>
-                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
-
-                </div>
-                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
-
-                </div>
-                <div className="card1 h-full min-w-[300px] bg-blue-500 text-white p-4 rounded-xl">
-
-                </div>
-            </div>
+  return (
+    <div className="overflow-x-auto p-4 no-scrollbar">
+      <div className="flex h-auto gap-5 items-start flex-wrap sm:flex-nowrap">
+        <div className="h-full w-full max-w-sm bg-red-400 text-white p-4 rounded-xl flex-shrink-0">
+          <div className="flex justify-between items-center">
+            <h4 className="bg-red-600 font-bold px-4 py-1 rounded text-sm">
+              High
+            </h4>
+            <h4 className="font-medium text-sm">9 July 2025</h4>
+          </div>
+          <h1 className="text-2xl font-bold mt-5">Learn Javascript</h1>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+            voluptates natus non in nulla tempora similique dolores excepturi
+            iusto voluptatum! Veritatis incidunt expedita ex eligendi dolore quo
+            consequuntur facere recusandae.
+          </p>
         </div>
-    )
-}
+        <div className="h-full w-full max-w-sm bg-green-400 text-white p-4 rounded-xl flex-shrink-0">
+          <div className="flex justify-between items-center">
+            <h4 className="bg-red-600 font-bold px-4 py-1 rounded text-sm">
+              High
+            </h4>
+            <h4 className="font-medium text-sm">9 July 2025</h4>
+          </div>
+          <h1 className="text-2xl font-bold mt-5">Learn Javascript</h1>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+            voluptates natus non in nulla tempora similique dolores excepturi
+            iusto voluptatum! Veritatis incidunt expedita ex eligendi dolore quo
+            consequuntur facere recusandae.
+          </p>
+        </div>
+        <div className="h-full w-full max-w-sm bg-yellow-400 text-white p-4 rounded-xl flex-shrink-0">
+          <div className="flex justify-between items-center">
+            <h4 className="bg-red-600 font-bold px-4 py-1 rounded text-sm">
+              High
+            </h4>
+            <h4 className="font-medium text-sm">9 July 2025</h4>
+          </div>
+          <h1 className="text-2xl font-bold mt-5">Learn Javascript</h1>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+            voluptates natus non in nulla tempora similique dolores excepturi
+            iusto voluptatum! Veritatis incidunt expedita ex eligendi dolore quo
+            consequuntur facere recusandae.
+          </p>
+        </div>
+        <div className="h-full w-full max-w-sm bg-violet-400 text-white p-4 rounded-xl flex-shrink-0">
+          <div className="flex justify-between items-center">
+            <h4 className="bg-red-600 font-bold px-4 py-1 rounded text-sm">
+              High
+            </h4>
+            <h4 className="font-medium text-sm">9 July 2025</h4>
+          </div>
+          <h1 className="text-2xl font-bold mt-5">Learn Javascript</h1>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+            voluptates natus non in nulla tempora similique dolores excepturi
+            iusto voluptatum! Veritatis incidunt expedita ex eligendi dolore quo
+            consequuntur facere recusandae.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
