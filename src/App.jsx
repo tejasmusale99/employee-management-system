f4939e3d833879e5d8f63cfc61cb8d41774bcade
Tejas Musale <tejasmusale99@gmail.com>
2025-07-09 22:58:42 +0530

    admin dashboard ui with responsivness

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-// import LogIn from "./components/Auth/LogIn"
+import LogIn from "./components/Auth/LogIn"
+import { AdminDashboard } from "./components/Dashboard/AdminDashboard"
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard"
 
 function App() {
   return (
     <>
     {/* <LogIn /> */}
-    <EmployeeDashboard />
+    {/* <EmployeeDashboard /> */}
+    <AdminDashboard />
     </>
   )
 }
