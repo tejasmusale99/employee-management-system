08063262fd2879533ee3bef77d9b65f14ac9f471
Tejas Musale <tejasmusale99@gmail.com>
2025-07-27 20:11:41 +0530

    created completed task component

---
+++ src/components/TaskList/CompletedTask.jsx
@@ -0,0 +1,21 @@
+export const CompletedTask = () => {
+  return (
+    <div>
+      <div className="h-full w-full max-w-sm bg-yellow-400 text-white p-4 rounded-xl flex-shrink-0">
+        <div className="flex justify-between items-center">
+          <h4 className="bg-red-600 font-bold px-4 py-1 rounded text-sm">
+            High
+          </h4>
+          <h4 className="font-medium text-sm">9 July 2025</h4>
+        </div>
+        <h1 className="text-2xl font-bold mt-5">Learn Javascript</h1>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+          voluptates natus non in nulla tempora similique dolores excepturi
+          iusto voluptatum! Veritatis incidunt expedita ex eligendi dolore quo
+          consequuntur facere recusandae.
+        </p>
+      </div>
+    </div>
+  );
+};
