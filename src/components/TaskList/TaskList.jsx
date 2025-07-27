d8a06f4af07d4097854d20bb2189be0d1b374333
Tejas Musale <tejasmusale99@gmail.com>
2025-07-27 20:02:19 +0530

    created acceept task component

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,24 +1,12 @@
+import { AcceptTask } from "./AcceptTask";
+
 export const TaskList = ({data}) => {
-  console.log(data.tasks)
+  // console.log(data.tasks)
   return (
     <div className="overflow-x-auto p-4 no-scrollbar">
       <div className="flex h-auto gap-5 items-start flex-wrap sm:flex-nowrap">
-        <div className="h-full w-full max-w-sm bg-red-400 text-white p-4 rounded-xl flex-shrink-0">
-          <div className="flex justify-between items-center">
-            <h4 className="bg-red-600 font-bold px-4 py-1 rounded text-sm">
-              High
-            </h4>
-            <h4 className="font-medium text-sm">9 July 2025</h4>
-          </div>
-          <h1 className="text-2xl font-bold mt-5">Learn Javascript</h1>
-          <p className="text-sm mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
-            voluptates natus non in nulla tempora similique dolores excepturi
-            iusto voluptatum! Veritatis incidunt expedita ex eligendi dolore quo
-            consequuntur facere recusandae.
-          </p>
-        </div>
-        <div className="h-full w-full max-w-sm bg-green-400 text-white p-4 rounded-xl flex-shrink-0">
+        <AcceptTask AcceptTaskData={data}/>
+        {/* <div className="h-full w-full max-w-sm bg-green-400 text-white p-4 rounded-xl flex-shrink-0">
           <div className="flex justify-between items-center">
             <h4 className="bg-red-600 font-bold px-4 py-1 rounded text-sm">
               High
@@ -62,7 +50,7 @@ export const TaskList = ({data}) => {
             iusto voluptatum! Veritatis incidunt expedita ex eligendi dolore quo
             consequuntur facere recusandae.
           </p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/components/smallComponents/Header.jsx
+++ src/components/smallComponents/Header.jsx
@@ -25,7 +25,7 @@ const handleLogOut = () => {
         <h3 className="text-white font-medium text-2xl sm:text-3xl"> {userName.firstName} {userName.lastName}👋</h3>
       </div>
       <div>
-        <button  onClick={handleLogOut} className="bg-red-600 px-4 py-2 sm:px-5 sm:py-3 rounded text-white text-sm sm:text-base">
+        <button  onClick={handleLogOut} className="bg-red-600 px-4 py-2 sm:px-5 sm:py-3 rounded text-white text-sm sm:text-base cursor-pointer">
           Log Out
         </button>
       </div>
