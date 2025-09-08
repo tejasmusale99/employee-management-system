41d73bfbf8b73250d553cbf67cb60dabc25d7624
Tejas Musale <tejasmusale99@gmail.com>
2025-09-08 21:12:16 +0530

    created table to fetch data using useContext

--- src/components/smallComponents/AllTask.jsx
+++ src/components/smallComponents/AllTask.jsx
@@ -1,46 +1,29 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
+
 export const AllTask = () => {
-    return(
-        <div className="bg-[#1c1c1c p-5 rounded h-48 overflow-auto no-scrollbar mt-2">
-            <div className="bg-red-400 py-2 px-4 flex mb-2 justify-between rounded">
-                <h2>Tejas</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-            <div className="bg-blue-400 py-2 px-4 flex mb-2 justify-between rounded">
-                <h2>Tejas</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-            <div className="bg-yellow-400 py-2 px-4 flex mb-2 justify-between rounded">
-                <h2>Tejas</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-            <div className="bg-purple-400 py-2 px-4 flex mb-2 justify-between rounded">
-                <h2>Tejas</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-            <div className="bg-red-400 py-2 px-4 flex mb-2 justify-between rounded">
-                <h2>Tejas</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-            <div className="bg-yellow-400 py-2 px-4 flex mb-2 justify-between rounded">
-                <h2>Tejas</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-            <div className="bg-amber-500 py-2 px-4 flex mb-2 justify-between rounded">
-                <h2>Tejas</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-            <div className="bg-purple-400 py-2 px-4 flex mb-2 justify-between rounded">
-                <h2>Tejas</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-        </div>
-    )
-}  
+  const authData = useContext(AuthContext);
+  console.log(authData.employees);
+  return (
+    <div className="bg-[#1c1c1c p-5 rounded h-48 overflow-auto no-scrollbar mt-2">
+      <div className="bg-red-400 py-2 px-4 flex mb-2 justify-between rounded">
+        <h2>Employee Name</h2>
+        <h3>New Task</h3>
+        <h5>Active Task</h5>
+        <h5>Completed Task</h5>
+        <h5>Failed Task</h5>
+      </div>
+      {authData.employees.map((emp) => {
+        return (
+          <div className="bg-red-400 py-2 px-4 flex mb-2 justify-between rounded">
+            <h3>{emp.firstName}</h3>
+            <h3>{emp.taskCount.newTask}</h3>
+            <h3>{emp.taskCount.newTask}</h3>
+            <h3>{emp.taskCount.newTask}</h3>
+            <h3>{emp.taskCount.newTask}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
