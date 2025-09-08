5e101999d7da4f86a7d6a54b62425cfdb1f25a3d
Tejas Musale <tejasmusale99@gmail.com>
2025-09-08 22:22:17 +0530

    applied styling to element

--- src/components/smallComponents/AllTask.jsx
+++ src/components/smallComponents/AllTask.jsx
@@ -3,9 +3,9 @@ import { AuthContext } from "../../context/AuthProvider";
 
 export const AllTask = () => {
   const authData = useContext(AuthContext);
-  console.log(authData.employees);
+//   console.log(authData.employees);
   return (
-    <div className="bg-[#1c1c1c p-5 rounded h-48  mt-2">
+    <div className="bg-[#1c1c1c p-5 rounded mt-2">
       <div className="bg-red-400 py-2 px-4 flex mb-2 justify-between rounded">
         <h2>Employee Name</h2>
         <h3>New Task</h3>
@@ -16,12 +16,12 @@ export const AllTask = () => {
       <div className="overflow-auto h-[80%] no-scrollbar">
       {authData.employees.map((emp) => {
         return (
-          <div className="bg-red-400 py-2 px-4 flex mb-2 justify-between rounded">
-            <h3>{emp.firstName}</h3>
-            <h3>{emp.taskCount.newTask}</h3>
-            <h3>{emp.taskCount.active}</h3>
-            <h3>{emp.taskCount.completed}</h3>
-            <h3>{emp.taskCount.failed}</h3>
+          <div className="py-2 px-4 flex mb-2 justify-between rounded border-2 border-amber-100">
+            <h3 className="text-amber-50">{emp.firstName}</h3>
+            <h3 className="text-amber-400">{emp.taskCount.newTask}</h3>
+            <h3 className="text-amber-50">{emp.taskCount.active}</h3>
+            <h3 className="text-green-500">{emp.taskCount.completed}</h3>
+            <h3 className="text-red-600">{emp.taskCount.failed}</h3>
           </div>
         );
        
